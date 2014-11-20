a583927234f1d72c3a7f145e1d0325d52be47127
Scott Stephenson <yalbik@yahoo.com>
2014-11-20 12:57:08 -0800

    First commit

---
+++ FirstApp/Controllers/EditAccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstApp.Controllers
+{
+    public class EditAccountController : Controller
+    {
+        // GET: EditAccount
+        public ActionResult Index()
+        {
+            return View("EditAccount");
+        }
+    }
+}
